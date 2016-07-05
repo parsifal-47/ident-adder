427636d342186c5adf7d15b70ec909d184df0505
Renat Idrisov <ren@ngs.ru>
2016-07-05 23:58:17 +0600

    Starting

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.add_idents = function(text, line_comment, commas, brackets) {
+	var lines = text.split("\n");
+	var idents = [0];
+	var comma = null;
+	var result = "";
+	
+	for (var i in lines) {
+		
+	}
+}
