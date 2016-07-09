5f093f412649f9389c6107698d4489d0c6f18d7b
Renat Idrisov <ren@ngs.ru>
2016-07-09 11:47:59 +0600

    Add, not to loose

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-exports.add_idents = function(text, line_comment, commas, brackets) {
+exports.add_idents = function(text, ident_token, dedent_token, line_comment, commas, brackets) {
 	var lines = text.split("\n");
 	var idents = [0];
+	var bracket = [];
 	var comma = null;
 	var result = "";
 	
 	for (var i in lines) {
-		
+		var line = lines[i];
+		if (bracket.length == 0 && comma == null) {
+			
+		} else {
+			// eat brackets
+		}
 	}
 }
